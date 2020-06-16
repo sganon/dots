9c56ed910f262e0fd77433ee0fd5eea570c11fda
Simon Ganon <simon@blueboard.io>
2020-06-16 11:17:41 +0200

    feat: enable multi monitor setup for work

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -5,8 +5,12 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar-top.log
-polybar top >>/tmp/polybar-top.log 2>&1 &
-
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    echo "---" | tee -a /tmp/polybar-$m-top.log
+    MONITOR=$m polybar --reload top &
+  done
+else
+  polybar --reload top &
+fi
 echo "Bars launched..."
