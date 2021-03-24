933d7311fb788d60e327c5458058ab7e672a7fc3
Simon Ganon <simon.ganon@channeladvisor.com>
2021-03-24 09:25:18 +0100

    feat: remove private emacs conf

--- private_dot_emacs.d/go.el
+++
@@ -1,7 +0,0 @@
-(add-to-list 'exec-path "/home/sganon/go/bin")
-(setenv "GOPRIVATE" "git.blueboard.io/*")
-(use-package go-mode
-  :ensure t
-  :config
-  (setq gofmt-command "goimports")
-  (add-hook 'before-save-hook 'gofmt-before-save))
--- private_dot_emacs.d/typescript.el
+++
@@ -1,7 +0,0 @@
-(use-package typescript-mode
-  :ensure t
-  :config
-  (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
-  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode)))
-
-
