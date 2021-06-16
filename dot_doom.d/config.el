65d41affd2bfe40ee324c46d4557485df0946c13
Simon Ganon <simon.ganon@channeladvisor.com>
2021-06-16 22:37:35 +0200

    emacs: theme modif

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -25,7 +25,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-wilmersdorf)
+(setq doom-theme 'doom-ayu-mirage)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -58,6 +58,3 @@
  :g "C-j" #'evil-window-down
  :g "C-k" #'evil-window-up)
 
-;transparent adjustment
-(set-frame-parameter (selected-frame)'alpha '(80 . 80))
-(add-to-list 'default-frame-alist'(alpha . (80 . 80)))
